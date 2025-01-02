dcb240382c5445bd0b1a8e8c7b427aa16a0453b0
Daniel Adams <danieladams456@gmail.com>
2025-01-02 09:45:10 -0500

    test ensuring getScore() is package private

---
+++ src/test/java/com/github/danieladams456/statusvoter/AttributeVisibilityTest.java
@@ -0,0 +1,26 @@
+package com.github.danieladams456.statusvoter;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AttributeVisibilityTest {
+    /**
+     * We don't want to leak numbers out to consuming applications.
+     * @throws NoSuchMethodException if getScore does not exist
+     */
+    @Test
+    void testExternalPackagesCannotSeeScore() throws NoSuchMethodException {
+        Class<?> clazz = StatusClassification.class;
+        Method method = clazz.getDeclaredMethod("getScore");
+        int modifiers = method.getModifiers();
+
+        // package private should be false on all
+        assertThat(Modifier.isPublic(modifiers)).isFalse();
+        assertThat(Modifier.isPrivate(modifiers)).isFalse();
+        assertThat(Modifier.isProtected(modifiers)).isFalse();
+    }
+}
