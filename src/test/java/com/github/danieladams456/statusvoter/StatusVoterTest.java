43d9183f6158ad92c4e9721919a2bd81c7bfa88a
Daniel Adams <danieladams456@gmail.com>
2025-01-02 07:38:46 -0500

    use AssertJ

--- src/test/java/com/github/danieladams456/statusvoter/StatusVoterTest.java
+++ src/test/java/com/github/danieladams456/statusvoter/StatusVoterTest.java
@@ -2,10 +2,12 @@ package com.github.danieladams456.statusvoter;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class StatusVoterTest {
     @Test
     void testStatusInit(){
         StatusVoter voter = new StatusVoter();
-        assert voter.toString().equals("INITIAL");
+        assertThat(voter).hasToString("INITIAL");
     }
 }
